a7cbb8f3e253e233e8193c5a3da737ef5bbd0a6a
Harikrishnan <hkkrishn563@gmail.com>
2020-05-06 15:39:45 -0700

    initial finished styling

---
+++ src/pages/Homepage/homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './homepage.scss'
+import Directory from './../../components/directory/directory'
+
+
+const HomePage = ()=>(
+    <Directory/>
+)
+
+export default HomePage;
