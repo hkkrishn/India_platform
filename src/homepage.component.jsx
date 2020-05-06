a8879783600520bf807b75b7619affdef096e14a
Harikrishnan <hkkrishn563@gmail.com>
2020-05-06 14:41:36 -0700

    base skeleton

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,46 +1,48 @@
 import React from 'react'
+import './homepage.components.scss'
+
 
 const HomePage = ()=>(
     <div className = "homepage">
     <div className = "directory-menu">
     <div className = "menu-item">
         <div class="content">
-            <h1>Hats</h1>
-            <span>Shop Now</span>
+            <h1 className = "title">Hats</h1>
+            <span className = "subtitle">Shop Now</span>
         </div>
     </div>
     <div className = "menu-item">
         <div class="content">
-            <h1>Hats</h1>
-            <span>Shop Now</span>
-        </div>
-    </div>
-
-    <div className = "menu-item">
-        <div class="content">
-            <h1>Jackets</h1>
-            <span>Shop Now</span>
+            <h1 className = "title">Hats</h1>
+            <span className = "subtitle">Shop Now</span>
         </div>
     </div>
 
     <div className = "menu-item">
         <div class="content">
-            <h1>Sneakers</h1>
-            <span>Shop Now</span>
+            <h1 className = "title">Jackets</h1>
+            <span className = "subtitle">Shop Now</span>
         </div>
     </div>
 
     <div className = "menu-item">
         <div class="content">
-            <h1>Womens</h1>
-            <span>Shop Now</span>
+            <h1 className = "title">Sneakers</h1>
+            <span className = "subtitle">Shop Now</span>
         </div>
     </div>
 
     <div className = "menu-item">
         <div class="content">
-            <h1>Mens</h1>
-            <span>Shop Now</span>
+            <h1 className = "title">Womens</h1>
+            <span className = "subtitle">Shop Now</span>
+        </div>
+    </div>
+
+    <div className = "menu-item">
+        <div class="content">
+            <h1 className = "title">Mens</h1>
+            <span className = "subtitle">Shop Now</span>
         </div>
     </div>
 
